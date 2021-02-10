ee92e19b0fb051fa732cb5c000dd655087db92f2
Anders Bjerner <abjerner@skybrud.dk>
2021-02-10 21:29:12 +0100

    Introduced new CultureAttribute class
    The attribute class lets developers decorate their WebAPI to set a specific culture for the current thread.

---
+++ src/Skybrud.WebApi/Attributes/CultureAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Skybrud.WebApi.Attributes {
+
+    /// <summary>
+    /// Attribute used for setting the culture used for a WebAPI controller or method.
+    /// </summary>
+    public class CultureAttribute : ActionFilterAttribute {
+
+        /// <summary>
+        /// Gets the culture.
+        /// </summary>
+        public CultureInfo Culture { get; }
+
+        /// <summary>
+        /// Initializes a new instance based on the specified <paramref name="cultureName"/>.
+        /// </summary>
+        /// <param name="cultureName">The name of the culture - eg. <c>da-DK</c>.</param>
+        public CultureAttribute(string cultureName) {
+            Culture = CultureInfo.GetCultureInfo(cultureName);
+        }
+
+        /// <inheritdoc />
+        public override void OnActionExecuting(HttpActionContext actionContext) {
+            Thread.CurrentThread.CurrentCulture = Culture;
+            Thread.CurrentThread.CurrentUICulture = Culture;
+        }
+
+    }
+
+}
